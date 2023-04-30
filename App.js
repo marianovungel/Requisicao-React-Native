c02016530ff83587a0e17d7694b29a7a8024aa2c
marianovungel <vungem88@gmail.com>
2023-04-30 17:10:43 -0300

    res

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+  const [data, setData] = useState([])
+
+  const getData = async()=>{
+    try {
+      const res = await axios.get("https://backmiti.onrender.com/user");
+      setData(res.data)
+      console.log(res.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+ useEffect(()=>{
+  getData()
+ }, [])
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.center}>
+        <FlatList
+          data={data}
+          renderItem={({item}) => <Text style={styles.item}>{item._id}</Text>}
+        />
+        <Text>Test</Text>
+        <Text>Test</Text>
+        <Text>Test</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +40,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '100%',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  center:{
+    flex: 1,
+    width: '100%',
+    flexDirection: 'column',
+    backgroundColor: '#999',
+    color: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  item:{
+    color: "red",
+    marginTop: 50,
+  },
+  
 });
